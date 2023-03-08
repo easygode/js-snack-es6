52d2eb69a04920fba62511727f04548cbd103c49
easygode <bimalbenvenuti@gmail.com>
2023-03-08 17:50:48 +0100

    creato array con gli ospite in ordine

--- snack1/main.js
+++ snack1/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+//1. Creo una lista con un array contentente gli ospiti per ordine di posto
+
+const ospitiVip = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+]
+
+console.log([ospitiVip]);
+
+// 2. Per ogni ospite creo un oggetto contenente:
+//     - nome del tavolo
+//     - nome dell'ospite
+//     - posto occupato
+// 3. Stampo a console la lista degli ospiti
