290bafc1cf3351e008ca022f20fd4454147394c4
easygode <bimalbenvenuti@gmail.com>
2023-03-09 18:54:05 +0100

    snack 4 - squadre create

--- snack4/main.js
+++ snack4/main.js
@@ -1,1 +1,47 @@
 'use strict';
+// 1. Creo un array di oggetti di squadre di calcio
+// 2. Agiungo  per ogni squadra le proprietà:
+//    nome, punti fatti, falli subiti.
+
+const squadre = [
+    {
+        nome: "Manchester",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Glasgow",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Roma",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Atalanta",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Galatasaray",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Salernitana",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Djurgarden",
+        punti: 0,
+        falli: 0
+    }
+    {
+        nome: "Gefle",
+        punti: 0,
+        falli: 0
+    }
+];
